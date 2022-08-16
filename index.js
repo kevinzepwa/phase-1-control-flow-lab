56cbb701ab45a2fa9ab281c56e25c9703fc581c1
kevinzepwa <zepwakevin8@gmail.com>
2022-08-16 12:52:31 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride <= 400){
+    return 'This one is on me!'
+  } else if (ride > 2000 && ride < 2500){
+    return 'I will gladly take your thirty bucks.'
+  } else {
+    return 'No can do.' 
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return (city == "NYC" ? "Ok, sounds good." : "No go.")
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+   switch (tip){
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    default:
+      return "Bye.";
+      break;
+  }
 }
